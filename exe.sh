3dda97b4894c9c6768add3fc71b3200c8ec72807
Everaldo-Gomes <everaldo332@gmail.com>
2023-05-04 11:10:09 -0300

    Code refactoring

--- exe.sh
+++ exe.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-make
-make run inst=0 config=9 exec_num=1
-make run inst=0 config=9 exec_num=2
-make run inst=0 config=9 exec_num=3
-make run inst=0 config=9 exec_num=4
 
+inst_qnt=15
+config_qnt=80
+exec_qnt=10
 
-
+for ((inst=1; inst<=15; inst++))
+do
+    for ((config=0; config<$config_qnt; config++))
+    do
+        for ((exec=1; exec<=$exec_qnt; exec++))
+        do
+            make run inst=$inst config=$config exec_num=$exec
+        done
+    done
+done
